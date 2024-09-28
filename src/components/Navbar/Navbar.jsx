516ff7d51afd3fc96c83d8ecc894f48b7080b6ad
lalit <lalitmohanty2002@gmail.com>
2024-09-28 11:00:20 +0530

    home page UI

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CooinContext'
+
+const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHandler = (event)=>{
+    switch (event.target.value){
+      case "usd": {
+        setCurrency({name:'usd', Symbol:"$"});
+        break;
+      }
+      case "eur": {
+        setCurrency({name:'eur', Symbol:"€"});
+        break;
+      }
+      case "inr": {
+        setCurrency({name:'inr', Symbol:"₹"});
+        break;
+      }
+      default: {
+        setCurrency({name:'inr', Symbol:"₹"});
+        break;
+      }
+      
+    }
+  }
+  return (
+    <div className='navbar'>
+      <img src={logo} alt=''/>
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className='nav-right'>
+        <select onChange={currencyHandleru}>
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="inr">INR</option>
+        </select>
+        <button>
+            Sign up <img src={arrow_icon} alt="" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
